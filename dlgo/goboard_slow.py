5747df776ef8e0df795168b87a014c5eee71f7db
saeed <abhari.saeed@gmail.com>
2020-12-11 17:02:19 +0330

    first lines of code

---
+++ dlgo/goboard_slow.py
@@ -0,0 +1,99 @@
+
+from dlgo.gotypes import Player
+
+class Move():
+    def __init__(self,point = None, is_pass =False, is_resign = False):
+        assert (point is not None) ^ is_pass ^ is_resign 
+        self.point = point
+        self.is_play = (self.point is not None)
+        self.is_pass = is_pass
+        self.is_resign = is_resign
+
+    
+    @classmethod
+    def play(cls, point):
+        return Move(point=point)
+
+    @classmethod
+    def pass_turn(cls):
+        return Move(is_pass = True)
+
+    @classmethod
+    def resign(cls):
+        return Move(is_resign = True)
+    
+
+class GoString():
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+    def merged_with(self, go_string: GoString):
+        assert self.color == go_string.color 
+        combined_stones = go_string.stones.union(self.stones)
+        return GoString(self.color, combined_stones, (self.liberties | go_string.liberties) - combined_stones)
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self,other):
+        return isinstance(other,GoString) and self.color == other.color and self.liberties == other.liberties and \
+            self.stones == other.stones
+
+    
+class Board():
+    def __init__(self,num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {}
+
+    def place_stone(self, player, point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+        adjacent_opposite_color = []
+        adjacent_same_color = []
+        liberties = []
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):
+                continue
+            neighbor_srting = self._grid.get(neighbor)
+            if neighbor_srting is None :
+                liberties.append(neighbor)
+            elif neighbor_srting.color == player:
+                if neighbor_srting not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_srting)
+            else:
+                if neighbor_srting not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_srting)
+        
+        new_string = GoString(player, [point], liberties)
+
+    def is_on_grid(self, point):
+        return 1 <= point.row <= self.num_rows and 1 <= point.col <= self.num_cols
+
+    def get(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.color
+
+    def get_go_string(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string
+
+
+
+
+
+
+    
